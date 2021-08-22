ec28b9605aefb437231581d74a3d3ab3a9278b00
Sajibulislam098 <sajibul.islam098@gmail.com>
2021-08-22 16:01:02 +0600

    complete

--- js/app.js
+++ js/app.js
@@ -1,20 +1,20 @@
 
 // update btn price function
-function upadatePrice(type, value, costValue){
-    if(value == 'high'){
+function upadatePrice(type, value, costValue) {
+    if (value == 'high') {
         document.getElementById(type + '_cost').innerText = costValue;
     }
-    else if(value == 'meduim'){
+    else if (value == 'meduim') {
         document.getElementById(type + '_cost').innerText = costValue;
     }
-    else{
+    else {
         document.getElementById(type + '_cost').innerText = costValue;
     }
     totalPrice();
 }
 
 // upadate total price function
-function totalPrice(){
+function totalPrice() {
     const memoryCost = document.getElementById('memory_cost').innerText;
     const storageCost = document.getElementById('storage_cost').innerText;
     const delivaryCost = document.getElementById('delivary_cost').innerText;
@@ -23,46 +23,46 @@ function totalPrice(){
     document.getElementById('total_cost_fainal').innerText = total;
 }
 
-// memory cost btn
-document.getElementById('default_cost_memory_btn').addEventListener('click', function(){
+// memory cost button
+document.getElementById('default_cost_memory_btn').addEventListener('click', function () {
     upadatePrice('memory', 'low', 0);
 })
-document.getElementById('extra_cost_memory_btn').addEventListener('click', function(){
+document.getElementById('extra_cost_memory_btn').addEventListener('click', function () {
     upadatePrice('memory', 'high', 180);
 })
 
-// storage cost btn
-document.getElementById('default_cost_storage_btn').addEventListener('click', function(){
+// storage cost button
+document.getElementById('default_cost_storage_btn').addEventListener('click', function () {
     upadatePrice('storage', 'low', 0);
 })
-document.getElementById('semi_extra_cost_storage_btn').addEventListener('click', function(){
+document.getElementById('semi_extra_cost_storage_btn').addEventListener('click', function () {
     upadatePrice('storage', 'meduim', 100);
 })
-document.getElementById('extra_cost_storage_btn').addEventListener('click', function(){
+document.getElementById('extra_cost_storage_btn').addEventListener('click', function () {
     upadatePrice('storage', 'high', 180);
 })
 
-// delivary cost btn
-document.getElementById('default_cost_delivary_btn').addEventListener('click', function(){
+// delivary cost button
+document.getElementById('default_cost_delivary_btn').addEventListener('click', function () {
     upadatePrice('delivary', 'low', 0);
 })
-document.getElementById('advance_cost_delivary_btn').addEventListener('click', function(){
+document.getElementById('advance_cost_delivary_btn').addEventListener('click', function () {
     upadatePrice('delivary', 'high', 20);
 })
 
 
 
 // promo code field 
-function promoCode(pCode){
-    if(pCode == 'stevekaku'){
+function promoCode(pCode) {
+    if (pCode == 'stevekaku') {
         const totalCost = document.getElementById('total_cost').innerText;
-        const afterApply = parseInt(totalCost) - ((parseInt(totalCost)/100)*20);
+        const afterApply = parseInt(totalCost) - ((parseInt(totalCost) / 100) * 20);
         document.getElementById('total_cost_fainal').innerText = afterApply;
         document.getElementById('promoCodeText').value = '';
     }
 }
 
-document.getElementById('promo_code_btn').addEventListener('click', function(){
+document.getElementById('promo_code_btn').addEventListener('click', function () {
     let inputField = document.getElementById('promoCodeText').value;
     promoCode(inputField)
 })
